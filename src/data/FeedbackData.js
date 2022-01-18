9799bf30f0b5e7f787c23f0cd9db8539153fe3aa
Carolina Nonato <csnonato@gmail.com>
2022-01-18 15:48:45 -0500

    feedbacks are displaying

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,23 @@
+const FeedbackData = [
+    {
+    id: 1,
+    rating: 10,
+    text: `It’s Cuz of that !W#$ ‘pizza’ that everyone down here is suffering. – Barret Wallace`,
+    },
+
+    {
+    id: 2,
+    rating: 9,
+    text: `“It’s like this train. It can’t run anywhere except where its rails take it.  – Cloud Strife`,
+    },
+
+
+    {
+    id: 3,
+    rating: 7,
+    text: `Words aren’t the only thing that tell people what you’re thinking.  – Tifa Lockheart`,
+    }
+
+]
+
+export default FeedbackData
