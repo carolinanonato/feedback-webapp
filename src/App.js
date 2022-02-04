3ef559e333e6d121eba3451e04b2018b3f7542bb
Carolina Nonato <csnonato@gmail.com>
2022-02-04 10:51:41 -0500

    Creating Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {v4 as uuidv4} from 'uuid'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import { useState } from "react";
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
 import Header from "./components/Header"
 import FeedbackData from "./data/FeedbackData";
+import AboutPage from "./pages/AboutPage";
 
 
 function App () {
@@ -27,16 +29,23 @@ const deleteFeedback = (id) => {
     
     
     return (
-        <>
+    <Router>
         <Header />
         <div className="container">
-            <FeedbackForm handleAdd={addFeedback}/>
-            <FeedbackStats feedback={feedback} />
-           <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
-
-           
-                </div>
+            <Routes>
+            <Route exact path='/' element={
+                <>
+                 <FeedbackForm handleAdd={addFeedback}/>
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
                 </>
+            }>
+       
+        </Route>
+        <Route path='/about' element={<AboutPage />} />
+        </Routes>
+        </div>
+    </Router>
     )
 }
 
