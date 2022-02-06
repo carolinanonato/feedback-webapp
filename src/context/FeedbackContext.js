4b114c67d94b9965e61d2a2f637b3352bd31ba82
Carolina Nonato <csnonato@gmail.com>
2022-02-06 17:49:38 -0500

    bug fix

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,75 +1,73 @@
-import { createContext, useState } from "react";
-import {v4 as uuidv4} from 'uuid'
-const FeedbackContext = createContext ()
+import { v4 as uuidv4 } from 'uuid'
+import { createContext, useState } from 'react'
 
+const FeedbackContext = createContext()
 
-export const FeedbackProvider = ({children}) => {
-    const [feedback, setFeedback] = useState ([
-        {
-            id: 1,
-            text: 'This item is from context 1',
-            rating: 9
-        },
-        {
-            id: 1,
-            text: 'This item is from context 2',
-            rating: 10
-        },
-        {
-            id: 1,
-            text: 'This item is from context 3',
-            rating: 7
-        },
-    ])
+export const FeedbackProvider = ({ children }) => {
+  const [feedback, setFeedback] = useState([
+    {
+      id: 1,
+      text: 'This is feedback item 1',
+      rating: 10,
+    },
+    {
+      id: 2,
+      text: 'This is feedback item 2',
+      rating: 9,
+    },
+    {
+      id: 3,
+      text: 'This is feedback item 3',
+      rating: 7,
+    },
+  ])
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  })
 
-    const [feedbackEdit, setFeedbackEdit] = useState ({
-        item: {},
-        edit: false,
-    }); 
+  // Add feedback
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
 
-    const addFeedback = (newFeedback) =>{
-        newFeedback.id = uuidv4()
-        setFeedback([newFeedback,...feedback])
-    
-        console.log(newFeedback)
+  // Delete feedback
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id))
     }
+  }
 
-    
-    const deleteFeedback = (id) => {
-        if(window.confirm('Are you sure you want to delete?')) {
-            setFeedback(feedback.filter((item) => item.id !== id))
-        }
-    }
+  // Update feedback item
+  const updateFeedback = (id, updItem) => {
+    setFeedback(
+      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+    )
+  }
 
-
-
-    const updateFeedback = (id, updItem) => {   
-        setFeedback(
-            feedback.map( (item) => (item.id === id ? {...item, ...updItem} : item))
-        
-        )
-    }
-
-
-    const editFeedback = (item) => {
+  // Set item to be updated
+  const editFeedback = (item) => {
     setFeedbackEdit({
-        item,
-        edit: true
+      item,
+      edit: true,
     })
-    }
+  }
 
-
-    return <FeedbackContext.Provider value={{
+  return (
+    <FeedbackContext.Provider
+      value={{
         feedback,
+        feedbackEdit,
         deleteFeedback,
         addFeedback,
         editFeedback,
-        feedbackEdit,
         updateFeedback,
-
-    }}>
-                {children}
-            </FeedbackContext.Provider>
+      }}
+    >
+      {children}
+    </FeedbackContext.Provider>
+  )
 }
 
-export default FeedbackContext                                                                                                                                             
+export default FeedbackContext
